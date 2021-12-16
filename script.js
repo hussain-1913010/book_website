cb74db6db62b3c73be6544e3375109aeef803e70
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-12-16 22:18:19 +0600

    featured section done

--- script.js
+++ script.js
@@ -50,3 +50,32 @@ var swiper = new Swiper(".booksSlider", {
       },
     },
   });
+
+
+  var swiper = new Swiper(".featuredSlider", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
