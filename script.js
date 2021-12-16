67cf32d66941f5e1a070a8f71d7e3c7359bb7cd4
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-12-16 23:47:57 +0600

    clients review done

--- script.js
+++ script.js
@@ -100,3 +100,28 @@ var swiper = new Swiper(".booksSlider", {
       },
     },
   });
+
+
+  
+  var swiper = new Swiper(".reviewSlider", {
+    spaceBetween: 10,
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
